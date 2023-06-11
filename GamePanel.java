419d329221c45f1620553894adf7d2ad6171593e
Balaji K <131997830+SaintBuddha1009@users.noreply.github.com>
2023-06-11 18:56:42 +0530

    Update GamePanel.java
    Changes colour of snake body

--- GamePanel.java
+++ GamePanel.java
@@ -12,7 +12,7 @@ public class GamePanel extends JPanel implements ActionListener {
 	static final int SCREEN_HEIGHT = 600;
 	static final int UNIT_SIZE = 25;
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT/UNIT_SIZE);
-	static final int DELAY = 100;
+	static final int DELAY = 90;
 	
 	final int x[] = new int[GAME_UNITS];
 	final int y[] = new int[GAME_UNITS];
@@ -57,12 +57,11 @@ public class GamePanel extends JPanel implements ActionListener {
 			
 			for(int i = 0; i<bodyParts; i++) {
 				if(i == 0) {
-					g.setColor(Color.green);
+					g.setColor(Color.blue);
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 				}
 				else {
 					g.setColor(new Color(45,180,0));
-					//g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 				}
 			}
